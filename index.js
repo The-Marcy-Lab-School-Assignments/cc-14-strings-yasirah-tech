406771f1a2bab1448d4ca00cc7239a83100795ad
Yasirah <yasirahboyce1@gmail.com>
2021-11-10 15:55:59 +0000

    cc-14

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//write your code here
+function stripUpperCase (string) {
+  let newStr = "";
+  for (let i = 0; i < string.length; i++){
+    if (string[i] === string[i].toLowerCase()){
+       newStr += string[i];
+    }
+  }
+  return newStr
+}
+   console.log(stripUpperCase("YayaAndJarae!!!BBHH"));
+
